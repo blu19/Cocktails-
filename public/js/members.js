0bb06e227edef5a5c20e511730e1e27280bbf2f9
ink <eric.zmo2@gmail.com>
2020-04-14 18:34:02 -0700

    for pull

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
-  });
+  function loginUser(email, password) {
+    $.post("/login", {
+      email: email,
+      password: password
+    })
+      .then(function () {
+        window.location.replace("/members");
+        // If there's an error, log the error
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
 });
