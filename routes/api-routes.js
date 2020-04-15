35e50c9d5ec3f794343ca75d3a9ef8ddb330f8dc
ink <eric.zmo2@gmail.com>
2020-04-14 21:28:22 -0700

    Logics for favs

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,7 +16,7 @@ module.exports = function (app) {
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/signup", function (req, res) {
-      console.log(req.body)
+    console.log(req.body)
     db.User.create({
       user_name: req.body.user_name,
       email: req.body.email,
@@ -34,10 +34,10 @@ module.exports = function (app) {
       });
   });
 
-  
+
   //ROAD BLOCK TO ASK FOR TA'S AND WILL 
 
-  app.post("/members",function(req, res){
+  app.post("/members", function (req, res) {
     console.log(req.body.id)
     db.User.update(
       req.body,
@@ -45,7 +45,7 @@ module.exports = function (app) {
         where: {
           id: req.body.id
         }
-    })
+      })
   });
 
   // Route for logging user out
@@ -55,28 +55,30 @@ module.exports = function (app) {
   });
 
   // Route for getting some data about our user to be used client side
-  app.get("/user_data", function (req, res) {
+  app.get("/favorites", function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
     } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id,
+      console.log(db.LikedDrinks)
+      db.LikedDrinks.findAll({
+        where: {
+          userId: req.user.id
+        },
+      }).then(function (LikedDrinks) {
+        res.json(LikedDrinks);
       });
     }
   });
-      app.post("/updatelikes", function(req, res){
-        console.log("----------------------------------------------------")
-        console.log(req.body)
-        console.log(req.body.name);
-        db.LikedDrinks.create({ 
-          userId: req.user.id,
-          drinksId: req.body.id,
-          drinksName: req.body.name         
-          })
-          .then(data => res.json(data))
-      });
+  app.post("/updatelikes", function (req, res) {
+    console.log("----------------------------------------------------")
+    console.log(req.body)
+    console.log(req.body.name);
+    db.LikedDrinks.create({
+      userId: req.user.id,
+      drinksId: req.body.id,
+      drinksName: req.body.name
+    })
+      .then(data => res.json(data))
+  });
 };
